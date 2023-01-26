93ee9dda61fcaab9264e8b54ba7e2ade11ae0cdb
John Quinn <johnjquinn01@outlook.com>
2023-01-26 16:42:22 -0500

    delete fix

--- src/routes/destroy.jsx
+++ src/routes/destroy.jsx
@@ -2,7 +2,6 @@ import { redirect } from "react-router-dom";
 import { deleteDream } from "../dream";
 
 export async function action({params}){
-    throw new Error("Oopsies!");
     await deleteDream(params.dreamId);
     return redirect('/');
 }
